4acb932f822bb71be276e2a330954e366da322ca
kratos <mhamedlehbab@gmail.com>
2021-09-26 15:40:39 +0100

    Implemented LocalDate

---
+++ src/components/Date.jsx
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+
+import "../styles/Date.css";
+
+
+function dayFormatter(date) {
+    let today = "";
+    switch (date) {
+        case 0:
+            today = "Saturday";
+            break;
+
+        case 1:
+            today = "Sunday";
+            break;
+
+        case 2:
+            today = "Monday";
+            break;
+
+        case 3:
+            today = "Tuesday";
+            break;
+
+        case 4:
+            today = "Wednesday";
+            break;
+
+        case 5:
+            today = "Thursday";
+            break;
+
+        case 6:
+            today = "Friday";
+            break;
+
+        default:
+            today = "--";
+            break;
+    }
+
+    return today;
+}
+
+function timeFormatter(time) {
+
+    const hours = time.split(':')[0];
+    const minutes = time.split(':')[1];
+
+    return hours + ":" + minutes;
+}
+
+class LocalDate extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { date: new Date() };
+    }
+
+    componentDidMount() {
+        this.timerID = window.setInterval(
+            () => this.tick(), 1000 * 60
+        );
+    }
+
+    componentWillUnmount() {
+        window.clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({ date: new Date() });
+    }
+
+
+    render() {
+
+        const _date = this.state.date;
+        const day = dayFormatter(_date.getDay());
+        const time = timeFormatter(_date.toTimeString());
+
+        console.log();
+
+        return (
+            <div className="bubble-date">
+                <p>{day}</p>
+                <span>{time}</span>
+            </div>
+        );
+    }
+}
+
+
+export default LocalDate;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,15 +2,50 @@ import React from "react";
 import Navbar from "./Navbar.jsx";
 
 import "../styles/Home.css";
+import LocalDate from "./Date.jsx";
 
 function Home() {
     return (<section className="home-section">
         <Navbar />
-        {/* <div className="today-container">
+        <div className="today-container">
             <div className="highlight-title">
                 <h1>Today's Highlights</h1>
             </div>
-        </div> */}
+
+            <div className="content">
+                <div className="card-bubble">
+                    <LocalDate />
+                    <div className="bubble-temp">
+                        <p>-99C</p>
+                    </div>
+                </div>
+
+                <div className="card-info">
+                    <div className="card-body">
+                        UX index
+                    </div>
+
+                    <div className="card-body">
+                        Wind Status
+                    </div>
+
+                    <div className="card-body">
+                        Sunrise and Sunset
+                    </div>
+
+                    <div className="card-body">
+                        Humidity
+                    </div>
+                    <div className="card-body">
+                        Visibility
+                    </div>
+
+                    <div className="card-body">
+                        Air Quality
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>);
 }
 
